5fb421b3613185e558ba7cea12d931ac8869172a
Dilip <dilipkumargupta121@gmail.com>
2017-09-09 11:50:53 +0530

    this is changes committed and pushed by new_branchcode

--- MyCollectionViewDemo/AppDelegate.swift
+++ MyCollectionViewDemo/AppDelegate.swift
@@ -13,7 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    // This is my new chenges
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
